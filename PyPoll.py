b04ebff5868824f48e6fdabfcbafa38b44c6ac99
Joshua Severin <69886887+jseverin1984@users.noreply.github.com>
2020-09-29 12:26:40 -0500

    updated code to print results to txt

--- PyPoll.py
+++ PyPoll.py
@@ -36,44 +36,52 @@ with open(file_to_load) as election_data:
 # The total number of votes each candidate won
         candidate_votes[candidate_name] += 1
 
-# Iterate through candidate list
-for candidate_name in candidate_votes:
-
-# retrieve vote count of candidate
-    votes = candidate_votes[candidate_name]
-
-# The percentage of votes each candidate won
-    vote_percentage = float(votes) / float(total_votes) * 100
-
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
-
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n"
-)
-
-print(winning_candidate_summary)
-
-
-
-
-
-# 5. The winner of the election based on popular vote
-
-
 with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("-------------------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+    election_results = (
+        f'\nElection Results\n'
+        f'-------------------------\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'-------------------------\n'
+    )
+    print(election_results, end="")
+
+    txt_file.write(election_results)
+
+
+    # Iterate through candidate list
+    for candidate_name in candidate_votes:
+
+    # retrieve vote count of candidate
+        votes = candidate_votes[candidate_name]
+
+    # The percentage of votes each candidate won
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+    #  print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # The winner of the election based on popular vote
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n"
+    )
+
+    print(winning_candidate_summary)
+
+
+
+    with open(file_to_save, "w") as txt_file:
+        txt_file.write("Counties in the Election\n")
+        txt_file.write("-------------------------\n")
+        txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
 
