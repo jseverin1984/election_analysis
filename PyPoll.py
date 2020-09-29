9348eafa1393bd32a1cee8b7f819bbf8c1d439c7
Joshua Severin <69886887+jseverin1984@users.noreply.github.com>
2020-09-29 00:09:37 -0500

    updated pypoll and added challenge starter code added analysis folder

--- PyPoll.py
+++ PyPoll.py
@@ -1,5 +1,32 @@
 # The data we need to retrieve.
+import os
+import csv
+
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader)
+    print(headers)
+
+    for row in file_reader:
+        print(row)
+
+
+
+
+with open(file_to_save, "w") as txt_file:
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
+
 # 1. The total number of voters cast
+
+
 # 2. A complete list of candidates who received votes
 # 3. The total number of votes each candidate won
 # 4. The percentage of votes each candidate won
